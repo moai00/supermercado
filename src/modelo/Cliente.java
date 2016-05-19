620f6aa469779c5bacafcf89b371cd2d0025cdf8
moai00 <miquelport@gmail.com>
2016-05-19 11:47:07 +0200

    boutique falta afegir i treure stock

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,31 @@
+/*
+ * entidad cliente
+ */
+package modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author usu21
+ */
+public class Cliente implements Serializable {
+    
+        private String nif;
+    
+
+    
+
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    
+    
+    
+}
---
+++ src/supermercado/Supermercado.java
@@ -0,0 +1,19 @@
+/*
+ * supermercado simplificado
+ */
+package supermercado;
+
+/**
+ *
+ * @author moai
+ */
+public class Supermercado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
